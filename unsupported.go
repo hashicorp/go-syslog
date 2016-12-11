d47ddaa72078e25dac90fefe43ebfcd3f382cebb
Mateusz Gajewski <mateusz.gajewski@allegrogroup.com>
2016-12-11 17:03:39 +0100

    Add comment

--- unsupported.go
+++ unsupported.go
@@ -11,6 +11,7 @@ func NewLogger(p Priority, facility, tag string) (Syslogger, error) {
 	return nil, fmt.Errorf("Platform does not support syslog")
 }
 
+// DialLogger is used to construct a new Syslogger that establishes connection to remote syslog server
 func DialLogger(network, raddr string, p Priority, facility, tag string) (Syslogger, error) {
 	return nil, fmt.Errorf("Platform does not support syslog")
 }
